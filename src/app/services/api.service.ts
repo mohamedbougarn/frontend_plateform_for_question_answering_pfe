291a25e06d6c3e76693342d70c08b5bf4d71daec
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-10 11:06:51 +0100

    add service api

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  url = environment.URL_CORE;
+  headers = new HttpHeaders();
+
+  constructor(public http: HttpClient) { }
+
+
+
+  
+  AddApi(id_client:any,key:any){
+    this.headers.append('Content-Type', 'application/json');
+        return this.http
+          .put<any>(this.url+'api/add',{id_client,key})
+          .pipe(map(res => {  
+          return res ;                    
+    })); 
+}
+
+}
