1eee20391841e521be45f94d42b18f2818d44215
mohamedbougarn <mohamedbougarn@gmail.com>
2022-05-15 00:30:08 +0100

    add logout in sidbar component

--- src/app/partials/sidebar/sidebar.component.ts
+++ src/app/partials/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,12 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
 
   idclient :any ;
-  constructor() { }
+  constructor(public router : Router) { }
 
   ngOnInit(): void 
   {
     this.idclient = localStorage.getItem('id');
     console.log("id de client = "+this.idclient)
+  }
+
+
+  LogOut()
+  {
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 
 }
